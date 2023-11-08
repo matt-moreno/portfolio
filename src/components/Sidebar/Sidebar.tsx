b2f763f36055d3f60a785acd3856a9d136d453a2
Matt Moreno <morenomatthew83@gmail.com>
2023-11-07 21:50:04 -0800

    sidebar with icons

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { Link, NavLink } from "react-router-dom"
 import Profile from "../Profile/Profile"
+import { FiHome, FiUser, FiCamera, FiMail } from "react-icons/fi"
 import "./Sidebar.css"
 
 export default function Sidebar() {
@@ -9,13 +10,24 @@ export default function Sidebar() {
                 <Profile />
             </Link>
             <nav>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/about">About</NavLink>
-                <NavLink to="/photos">Photos</NavLink>
+                <div>
+                    <FiHome />
+                    <NavLink to="/">Home</NavLink>
+                </div>
+                <div>
+                    <FiUser />
+                    <NavLink to="/about">About</NavLink>
+                </div>
+                <div>
+                    <FiCamera />
+                    <NavLink to="/photos">Photos</NavLink>
+                </div>
+                <div>
+                    <FiMail />
+                    <NavLink to="/contact">Contact</NavLink>
+                </div>
             </nav>
-            <Link to="/contact" className="contact-button">
-                <button>Contact</button>
-            </Link>
+            {/* Sidebar footer goes here */}
         </header>
     )
 }
