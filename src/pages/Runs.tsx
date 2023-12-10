22a707f5d81c9540ed7d72859ef547b2eb27d80b
Matt Moreno <morenomatthew83@gmail.com>
2023-12-10 00:03:23 -0800

    fetch athlete data

--- src/pages/Runs.tsx
+++ src/pages/Runs.tsx
@@ -1,5 +1,34 @@
+import { useState, useEffect } from "react"
+
 export default function Runs() {
+    const [data, setData] = useState()
+    const url = 'https://www.strava.com/api/v3/athlete'
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch(url, {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${import.meta.env.VITE_STRAVA_ACCESS}`
+                }
+            });
+            const result = await response.json();
+            setData(result);
+          } catch (error) {
+            console.error('Error fetching data:', error);
+          }
+        };
+    
+        fetchData();
+      }, [])
+
+      console.log(data)
+
     return (
-        <h1>Strava API call???</h1>
+        <div>
+            <h1>Strava API call???</h1>
+        </div>
     )
 }
