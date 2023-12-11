143f95a3869f2ced9dd7c6794aec2fcda1abc2f6
Matt Moreno <morenomatthew83@gmail.com>
2023-12-10 16:44:56 -0800

    add the access token refresh

--- src/pages/Runs.tsx
+++ src/pages/Runs.tsx
@@ -2,29 +2,45 @@ import { useState, useEffect } from "react"
 
 export default function Runs() {
     const [data, setData] = useState()
-    const url = 'https://www.strava.com/api/v3/athlete'
 
     useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const response = await fetch(url, {
-                method: "GET",
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${import.meta.env.VITE_STRAVA_ACCESS}`
-                }
-            });
-            const result = await response.json();
-            setData(result);
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
-        };
-    
-        fetchData();
-      }, [])
+      const refreshAccessToken = async () => {
+        try {
+        const response = await fetch("https://www.strava.com/oauth/token", {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                client_id: import.meta.env.VITE_STRAVA_CLIENT_ID,
+                client_secret: import.meta.env.VITE_STRAVA_SECRET,
+                code: import.meta.env.VITE_STRAVA_AUTH_CODE,
+                grant_type: 'refresh_token',
+                refresh_token: import.meta.env.VITE_STRAVA_REFRESH_TOKEN
+            })
+        })
+        const result = await response.json()
+        fetchActivityData(result.access_token)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+        }    
+      }
 
-      console.log(data)
+      const fetchActivityData = async (token: string) => {
+        try {
+          const response = await fetch(`https://www.strava.com/api/v3/activities?access_token=${token}`);
+          const result = await response.json();
+          setData(result);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+
+      refreshAccessToken()
+    }, [])
+    
+    console.log(data)
 
     return (
         <div>
