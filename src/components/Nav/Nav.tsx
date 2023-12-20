813d105d12a254d80cc19e3d36a47e20dc2514a7
Matt Moreno <morenomatthew83@gmail.com>
2023-12-19 20:51:27 -0800

    make nav its own component

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,35 @@
+import { NavLink } from "react-router-dom"
+import { FiHome, FiUser, FiCamera, FiMail } from "react-icons/fi"
+import { FaRunning, FaRegLightbulb } from "react-icons/fa"
+import "./Nav.css"
+
+export default function Nav() {
+    return (
+        <nav>
+            <div className="nav-container">
+                <FiHome className="icon-style" />
+                <NavLink to="/">Home</NavLink>
+            </div>
+            <div className="nav-container">
+                <FiUser className="icon-style" />
+                <NavLink to="/about">About</NavLink>
+            </div>
+            <div className="nav-container">
+                <FiCamera className="icon-style" />
+                <NavLink to="/photos">Photos</NavLink>
+            </div>
+            <div className="nav-container">
+                <FaRunning className="icon-style" />
+                <NavLink to="/runs">Runs</NavLink>
+            </div>
+            <div className="nav-container">
+                <FaRegLightbulb className="icon-style" />
+                <NavLink to="/recommendations">Recommendations</NavLink>
+            </div>
+            <div className="nav-container">
+                <FiMail className="icon-style" />
+                <NavLink to="/contact">Contact</NavLink>
+            </div>
+        </nav>
+    )
+}
