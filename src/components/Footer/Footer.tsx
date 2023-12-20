96bf31448650acdde0813956bbc95596ac1db5d8
Matt Moreno <morenomatthew83@gmail.com>
2023-12-19 20:50:53 -0800

    add email to pop-up footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import "./Footer.css"
+
+export default function Footer() {
+    return (
+        <p className="email">
+            <a href="mailto:morenomatthew83@gmail.com">morenomatthew83@gmail.com</a>
+        </p>
+    )
+}
