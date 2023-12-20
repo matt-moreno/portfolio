4b6424d082af2e2026c6743d4ff90f6e24dacc81
Matt Moreno <morenomatthew83@gmail.com>
2023-12-19 22:26:07 -0800

    home placeholder content

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -21,13 +21,56 @@ export default function Home() {
 
     return (
         <div className="home-wrapper">
-            <div className="home-top">
-                <h1>Matt Moreno</h1>
-                <h2>I am a <span ref={textSpan}></span></h2>
-            </div>
-            <div>
+
+            <div className="hero">
+                <div className="home-content">
+                    <h1>Matt Moreno</h1>
+                    <h2>I am a <span ref={textSpan}></span></h2>
+                </div>
                 <img className="home-photo" src="src/assets/laptop.jpg" />
             </div>
+
+            <div className="experience">
+                <h3>Experience</h3>
+                <h4>Dubsado</h4>
+                <h5>Product Owner II</h5>
+                <p>Since 2022</p>
+                <ul>
+                    <li>experience bullet point</li>
+                    <li>experience bullet point</li>
+                    <li>experience bullet point</li>
+                    <li>experience bullet point</li>
+                    <li>experience bullet point</li>
+                </ul>
+                <h5>Product Owner I</h5>
+                <p>2021-2022</p>
+                <ul>
+                    <li>experience bullet point</li>
+                    <li>experience bullet point</li>
+                    <li>experience bullet point</li>
+                    <li>experience bullet point</li>
+                    <li>experience bullet point</li>
+                </ul>
+                <h5>Customer Success</h5>
+                <p>2020-2021</p>
+                <ul>
+                    <li>experience bullet point</li>
+                    <li>experience bullet point</li>
+                    <li>experience bullet point</li>
+                    <li>experience bullet point</li>
+                    <li>experience bullet point</li>
+                </ul>
+            </div>
+
+            <div className="education">
+                <h3>Education</h3>
+                <p>University of California, Riverside</p>
+                <p>Certified Scrum Product Owner</p>
+                <p>Scrimba certificate</p>
+                <p>Cybersecurity certificate</p>
+            </div>
+
+
         </div>
     )
 }
