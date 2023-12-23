b5c603506231ddeee82936c9b6a7e1ca17bf3b83
Matt Moreno <morenomatthew83@gmail.com>
2023-12-23 15:12:38 -0800

    create express server

---
+++ api/app.js
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+const express = require('express');
+const app = express();
+const port = 3000; // You can choose any available port
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
