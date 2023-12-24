69da8d5dc4e9dda51fdaa7d77fb221706494d6c7
Matt Moreno <morenomatthew83@gmail.com>
2023-12-24 13:17:50 -0800

    map css

--- src/pages/Runs/Runs.tsx
+++ src/pages/Runs/Runs.tsx
@@ -22,7 +22,7 @@ export default function Runs() {
     console.log(data)
 
     return (
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} >
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} id="map">
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
