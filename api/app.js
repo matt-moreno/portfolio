45ad034aa6229e4b759fbcb093b82cf8b5318526
Matt Moreno <morenomatthew83@gmail.com>
2023-12-23 17:25:15 -0800

    make strava api call in express server

--- api/app.js
+++ api/app.js
@@ -1,13 +1,47 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
-const express = require('express');
-const app = express();
-const port = 3000; // You can choose any available port
+const express = require("express")
+const app = express()
+const port = 3000
+require('dotenv').config()
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.get("/api/stravaData", (req, res) => {
+  const refreshAccessToken = async () => {
+    try {
+    const response = await fetch("https://www.strava.com/oauth/token", {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            client_id: process.env.STRAVA_CLIENT_ID,
+            client_secret: process.env.STRAVA_SECRET,
+            code: process.env.STRAVA_AUTH_CODE,
+            grant_type: 'refresh_token',
+            refresh_token: process.env.STRAVA_REFRESH_TOKEN
+        })
+    })
+    const result = await response.json()
+    fetchActivityData(result.access_token)
+  } catch (error) {
+    console.error('Error fetching data:', error)
+    }    
+  }
+
+  const fetchActivityData = async (token) => {
+    try {
+      const response = await fetch(`https://www.strava.com/api/v3/activities?access_token=${token}`);
+      const result = await response.json();
+      res.send(result);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  refreshAccessToken()
+})
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+  console.log(process.env.STRAVA_CLIENT_ID);
 });
