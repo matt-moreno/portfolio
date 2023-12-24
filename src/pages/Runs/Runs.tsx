60ad74c0cde22225fb8f7f2a5b5b5b4e9b80fe48
Matt Moreno <morenomatthew83@gmail.com>
2023-12-24 13:01:49 -0800

    install react leaflet

--- src/pages/Runs/Runs.tsx
+++ src/pages/Runs/Runs.tsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react"
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import "./Runs.css"
 
 export default function Runs() {
     const [data, setData] = useState()
@@ -20,8 +22,16 @@ export default function Runs() {
     console.log(data)
 
     return (
-        <div>
-            <h1>Strava API call???</h1>
-        </div>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+      </MapContainer>
     )
 }
