e1ffce9a7038660a887fad375caa942a656aa240
Matt Moreno <morenomatthew83@gmail.com>
2023-12-24 14:34:21 -0800

    import polyline

--- src/pages/Runs/Runs.tsx
+++ src/pages/Runs/Runs.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import polyline from "@mapbox/polyline"
 import Loading from "../../components/Loading/Loading"
 import "./Runs.css"
 
@@ -27,17 +28,16 @@ export default function Runs() {
     if (loading) {
       return <Loading />
     }
-
     
     return (
       <div>
-        <h1>{data[0].map.summary_polyline}</h1>
-          <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} id="map">
+        <h1>This is my Strava Activity!</h1>
+          <MapContainer center={[34.0522, -118.2437]} zoom={13} scrollWheelZoom={false} id="map">
               <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
-              <Marker position={[51.505, -0.09]}>
+              <Marker position={[34.0522, -118.2437]}>
                 <Popup>
                   A pretty CSS3 popup. <br /> Easily customizable.
                 </Popup>
