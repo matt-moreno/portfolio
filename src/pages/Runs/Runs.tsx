34f1d3d14e92c5db0737f90f668d616c416f4926
Matt Moreno <morenomatthew83@gmail.com>
2023-12-23 17:48:27 -0800

    make api call on the frontend

--- src/pages/Runs/Runs.tsx
+++ src/pages/Runs/Runs.tsx
@@ -4,40 +4,17 @@ export default function Runs() {
     const [data, setData] = useState()
 
     useEffect(() => {
-      const refreshAccessToken = async () => {
+      const getStravaData = async () => {
         try {
-        const response = await fetch("https://www.strava.com/oauth/token", {
-            method: 'post',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                client_id: import.meta.env.VITE_STRAVA_CLIENT_ID,
-                client_secret: import.meta.env.VITE_STRAVA_SECRET,
-                code: import.meta.env.VITE_STRAVA_AUTH_CODE,
-                grant_type: 'refresh_token',
-                refresh_token: import.meta.env.VITE_STRAVA_REFRESH_TOKEN
-            })
-        })
-        const result = await response.json()
-        fetchActivityData(result.access_token)
+          const response = await fetch("http://localhost:3000/api/stravaData")
+          const result = await response.json()
+          setData(result)
       } catch (error) {
-        console.error('Error fetching data:', error)
+          console.error('Error fetching data:', error)
         }    
       }
 
-      const fetchActivityData = async (token: string) => {
-        try {
-          const response = await fetch(`https://www.strava.com/api/v3/activities?access_token=${token}`);
-          const result = await response.json();
-          setData(result);
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      };
-
-      refreshAccessToken()
+      getStravaData()
     }, [])
     
     console.log(data)
