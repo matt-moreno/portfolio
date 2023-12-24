b7fd00840e7dcc6dd2a6fd34102ab6b7470ee3b0
Matt Moreno <morenomatthew83@gmail.com>
2023-12-24 13:59:05 -0800

    loading component

--- src/pages/Runs/Runs.tsx
+++ src/pages/Runs/Runs.tsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react"
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import Loading from "../../components/Loading/Loading"
 import "./Runs.css"
 
 export default function Runs() {
     const [data, setData] = useState()
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
       const getStravaData = async () => {
@@ -13,25 +15,34 @@ export default function Runs() {
           setData(result)
       } catch (error) {
           console.error('Error fetching data:', error)
-        }    
+        } finally {
+          setLoading(false)
+        } 
       }
 
       getStravaData()
     }, [])
-    
-    console.log(data)
 
+    // TODO: CREATE LOADING COMPONENT
+    if (loading) {
+      return <Loading />
+    }
+
+    
     return (
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} id="map">
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-      </MapContainer>
+      <div>
+        <h1>{data[0].map.summary_polyline}</h1>
+          <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} id="map">
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={[51.505, -0.09]}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+          </MapContainer>
+      </div>
     )
 }
