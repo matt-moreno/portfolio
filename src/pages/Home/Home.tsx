5a49d3dbfa27b20603ea2e5dfc93cf4aa9b81d6c
Matt Moreno <morenomatthew83@gmail.com>
2023-12-25 23:04:40 -0800

    add jointed bullet points

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -32,34 +32,57 @@ export default function Home() {
 
             <div className="experience">
                 <h3>Experience</h3>
-                <h4>Dubsado</h4>
-                <h5>Product Owner II</h5>
-                <p>Since 2022</p>
                 <ul>
-                    <li>experience bullet point</li>
-                    <li>experience bullet point</li>
-                    <li>experience bullet point</li>
-                    <li>experience bullet point</li>
-                    <li>experience bullet point</li>
+                    <li className="experience-item">
+                        <div className="marker-lg">
+                            <svg aria-hidden="true" viewBox="0 0 32 32" focusable="false">
+                            <path d="M16 4c6.6 0 12 5.4 12 12s-5.4 12-12 12S4 22.6 4 16 9.4 4 16 4zm0-4C7.2 0 0 7.2 0 16s7.2 16 16 16 16-7.2 16-16S24.8 0 16 0z"></path>
+                            <circle cx="16" cy="16" r="6"></circle>
+                            </svg>
+                        </div>
+                        <h4>Dubsado</h4>
+                        <h5>Product Owner II</h5>
+                        <p>Since 2022</p>
+                        <ul>
+                            <li>experience bullet point</li>
+                            <li>experience bullet point</li>
+                            <li>experience bullet point</li>
+                            <li>experience bullet point</li>
+                            <li>experience bullet point</li>
+                        </ul>
+                    </li>
+
+                    <li className="experience-item">
+                        <div className="marker-sm">
+                            <svg aria-hidden="true" viewBox="0 0 32 32" focusable="false"><circle stroke="none" cx="16" cy="16" r="10"></circle></svg>
+                        </div>
+                        <h5>Product Owner I</h5>
+                        <p>2021-2022</p>
+                        <ul>
+                            <li>experience bullet point</li>
+                            <li>experience bullet point</li>
+                            <li>experience bullet point</li>
+                            <li>experience bullet point</li>
+                            <li>experience bullet point</li>
+                        </ul>
+                    </li>
+
+                    <li className="experience-item">
+                        <div className="marker-sm">
+                            <svg aria-hidden="true" viewBox="0 0 32 32" focusable="false"><circle stroke="none" cx="16" cy="16" r="10"></circle></svg>
+                        </div>
+                        <h5>Customer Success</h5>
+                        <p>2020-2021</p>
+                        <ul>
+                            <li>experience bullet point</li>
+                            <li>experience bullet point</li>
+                            <li>experience bullet point</li>
+                            <li>experience bullet point</li>
+                            <li>experience bullet point</li>
+                        </ul>
+                    </li>
                 </ul>
-                <h5>Product Owner I</h5>
-                <p>2021-2022</p>
-                <ul>
-                    <li>experience bullet point</li>
-                    <li>experience bullet point</li>
-                    <li>experience bullet point</li>
-                    <li>experience bullet point</li>
-                    <li>experience bullet point</li>
-                </ul>
-                <h5>Customer Success</h5>
-                <p>2020-2021</p>
-                <ul>
-                    <li>experience bullet point</li>
-                    <li>experience bullet point</li>
-                    <li>experience bullet point</li>
-                    <li>experience bullet point</li>
-                    <li>experience bullet point</li>
-                </ul>
+
             </div>
 
             <div className="education">
