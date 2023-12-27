631e34ee8ff429efecad9091aafb971305475db7
Matt Moreno <morenomatthew83@gmail.com>
2023-12-26 22:51:09 -0800

    Change to resources

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import MainLayout from './layouts/MainLayout'
-import Home from './pages/Home/Home'
-import About from './pages/About/About'
-import Photos from './pages/Photos/Photos'
-import Contact from './pages/Contact/Contact'
-import Runs from "./pages/Runs/Runs"
-import Recommendations from './pages/Recommendations/Recommendations'
-import NotFound from './pages/NotFound/NotFound'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Photos from "./pages/Photos/Photos";
+import Contact from "./pages/Contact/Contact";
+import Runs from "./pages/Runs/Runs";
+import Resources from "./pages/Resources/Resources";
+import NotFound from "./pages/NotFound/NotFound";
+import "./App.css";
 
 function App() {
   return (
@@ -18,13 +18,14 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="photos" element={<Photos />} />
           <Route path="runs" element={<Runs />} />
-          <Route path="recommendations" element={<Recommendations />} />
-          <Route path="contact" element={<Contact />} /> 
-        </Route> 
+          {/* TODO: Change below to resources??? */}
+          <Route path="resources" element={<Resources />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
