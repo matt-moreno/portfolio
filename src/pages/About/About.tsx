ce9a7dae937eefd2b3c4122b6d02afc7083788e5
Matt Moreno <morenomatthew83@gmail.com>
2023-12-26 22:51:22 -0800

    Add some more content to about page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,5 +1,32 @@
+import "./About.css";
+
 export default function About() {
-    return (
-        <h1>This page may get deleted?? Perhaps a summary about yourself???</h1>
-    )
+  return (
+    <div className="about-wrapper">
+      <div className="about-top">
+        <img
+          src="/src/assets/matt-profile.jpg"
+          alt="Profile photo of Matt Moreno"
+        />
+        <div>
+          <p>About me</p>
+          <h1>Product Owner</h1>
+          <p>
+            I am a Product Owner at Dubsado and I like what I do. I need to use
+            a different picture for this section. Something professional, yet
+            warm and inviting. Something about running, something about Piano.
+            Plz hire me
+          </p>
+        </div>
+      </div>
+      <div className="about-middle">
+        <h2>Development</h2>
+        <div>HTML, CSS, Javascript, React, Typescript</div>
+      </div>
+      <div className="about-bottom">
+        <h2>Google Course Era Data Analytics</h2>
+        <p>Why? Because I want to be a data driven Product Owner</p>
+      </div>
+    </div>
+  );
 }
