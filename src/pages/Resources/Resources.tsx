1897801015c0c12bb78ef2d63b9ba872690ddb0f
Matt Moreno <morenomatthew83@gmail.com>
2023-12-30 18:19:04 -0800

    resource page layout

--- src/pages/Resources/Resources.tsx
+++ src/pages/Resources/Resources.tsx
@@ -1,21 +1,39 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import "./Resources.css";
 
 // TODO: CREATE A FILE THAT WILL HOLD ALL THE RESOURCE DATA, THEN MAP OVER IT IN THE COMPONENTS
 
 export default function Resources() {
   return (
-    <div>
+    <div className="resources-wrapper">
       <h1>
         Book Recommendations, Movie Recommendations, Website References, YouTube
         Videos
       </h1>
       <nav className="resources-nav">
-        <Link to="/resources">Websites</Link>
-        <Link to="/resources/videos">Videos</Link>
-        <Link to="/resources/books">Books</Link>
+        <NavLink
+          to="."
+          end
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Websites
+        </NavLink>
+        <NavLink
+          to="/resources/videos"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Videos
+        </NavLink>
+        <NavLink
+          to="/resources/books"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Books
+        </NavLink>
       </nav>
-      <Outlet />
+      <div className="card-grid">
+        <Outlet />
+      </div>
     </div>
   );
 }
