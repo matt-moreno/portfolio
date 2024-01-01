3153bb66e9a15dae081e8a8a42aabf70d4f055b4
Matt Moreno <morenomatthew83@gmail.com>
2023-12-31 16:52:06 -0800

    add react loading

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,3 +1,11 @@
+import ReactLoading from "react-loading";
+import "./Loading.css";
+
 export default function Loading() {
-    return <div>Loading, please wait...</div>
+  return (
+    <div className="loading">
+      <ReactLoading type="bars" />
+      <h1>Loading</h1>
+    </div>
+  );
 }
