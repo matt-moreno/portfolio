e00aca3c9612aea54ebaf57e66966014cad95481
Matt Moreno <morenomatthew83@gmail.com>
2024-02-15 18:38:30 -0800

    sidebar

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,13 +1,27 @@
-import { Outlet } from "react-router-dom"
-import Sidebar from "../components/Sidebar/Sidebar"
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import Menu from "../components/Menu/Menu";
+import Sidebar from "../components/Sidebar/Sidebar";
+
+export interface SidebarTypes {
+  isSidebarOpen?: boolean;
+  toggleSidebar: () => void;
+}
 
 export default function MainLayout() {
-    return (
-        <div className="site-wrapper">
-            <Sidebar />
-            <main>
-                <Outlet />
-            </main>
-        </div>
-    )
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <div className="site-wrapper">
+      <Menu isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
