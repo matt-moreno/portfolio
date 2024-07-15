2e7016f8d9953d841cb56d43beedf23848f9179b
Matt Moreno <morenomatthew83@gmail.com>
2024-07-15 00:09:19 -0700

    Projects page component

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,3 +1,35 @@
+import { NavLink, Outlet, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import "./Projects.css";
+import Card from "../../components/Card/Card";
+
 export default function Projects() {
-  return <h1>This is where my Kaggle case study will go </h1>;
+  const [isOutletActive, setIsOutletActive] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    // Check if the current path is not the root path to determine if Outlet is active
+    setIsOutletActive(location.pathname !== "/projects");
+  }, [location]);
+
+  return (
+    <div className="main-wrapper">
+      <h1>Projects</h1>
+      {isOutletActive ? (
+        <NavLink to="/projects">
+          <p>Go back</p>
+        </NavLink>
+      ) : (
+        <NavLink to="/projects/bellabeat-case-study">
+          <Card
+            title="Bellabeat Case Study"
+            description="Google Coursera Capstone Project"
+          />
+        </NavLink>
+      )}
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  );
 }
