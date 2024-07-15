19f2c1c45e449f34168088313aa29f7dc7a3f1ed
Matt Moreno <morenomatthew83@gmail.com>
2024-07-14 21:36:32 -0700

    prettier

--- src/pages/Runs/Runs.tsx
+++ src/pages/Runs/Runs.tsx
@@ -1,9 +1,9 @@
-import { useState, useEffect } from "react"
-import { MapContainer, Popup, TileLayer, Polyline } from 'react-leaflet'
-import { ActivityTypes } from "./RunTypes"
-import polyline from "@mapbox/polyline"
-import Loading from "../../components/Loading/Loading"
-import "./Runs.css"
+import { useState, useEffect } from "react";
+import { MapContainer, Popup, TileLayer, Polyline } from "react-leaflet";
+import { ActivityTypes } from "./RunTypes";
+import polyline from "@mapbox/polyline";
+import Loading from "../../components/Loading/Loading";
+import "./Runs.css";
 
 interface StravaTypes {
   id: number;
@@ -17,61 +17,67 @@ interface StravaTypes {
 }
 
 export default function Runs() {
-    const [loading, setLoading] = useState(true)
-    const [activities, setActivities] = useState<StravaTypes[]>([])
+  const [loading, setLoading] = useState(true);
+  const [activities, setActivities] = useState<StravaTypes[]>([]);
 
-    useEffect(() => {
-      const getStravaData = async () => {
-        try {
-          const response = await fetch("http://localhost:3000/api/stravaData")
-          const result = await response.json()
-          const stravaArr: StravaTypes[] = []
-          result.map((data: ActivityTypes) => {
-            stravaArr.push({
-              id: data.id,
-              title: data.name,
-              exercise: data.type,
-              elevHigh: data.elev_high,
-              elevLow: data.elev_low,
-              startLat: data.start_latlng,
-              endLat: data.end_latlng,
-              polyline: polyline.decode(data.map.summary_polyline)
-            })
-          })
-          setActivities(stravaArr)
+  useEffect(() => {
+    const getStravaData = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/stravaData");
+        const result = await response.json();
+        const stravaArr: StravaTypes[] = [];
+        result.map((data: ActivityTypes) => {
+          console.log(data);
+          stravaArr.push({
+            id: data.id,
+            title: data.name,
+            exercise: data.type,
+            elevHigh: data.elev_high,
+            elevLow: data.elev_low,
+            startLat: data.start_latlng,
+            endLat: data.end_latlng,
+            polyline: polyline.decode(data.map.summary_polyline),
+          });
+        });
+        setActivities(stravaArr);
       } catch (error) {
-          console.error('Error fetching data:', error)
-        } finally {
-          setLoading(false)
-        } 
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
       }
+    };
 
-      getStravaData()
-    }, [])
+    getStravaData();
+  }, []);
 
-    // TODO: CREATE LOADING COMPONENT
-    if (loading) {
-      return <Loading />
-    }
+  // TODO: CREATE LOADING COMPONENT
+  if (loading) {
+    return <Loading />;
+  }
 
-    return (
-      <div>
-        <h1>This is my Strava Activity!</h1>
-          <MapContainer center={[34.0522, -118.2437]} zoom={12} scrollWheelZoom={false} id="map">
-              <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-              {activities.map((activity) => (
-                <Polyline key={activity.id} positions={activity.polyline}>
-                  <Popup>
-                    <div>
-                      <h2>{activity.exercise + ": " + activity.title}</h2>
-                    </div>
-                  </Popup>
-                </Polyline>
-              ))}
-          </MapContainer>
-      </div>
-    )
+  return (
+    <div>
+      <h1>This is my Strava Activity!</h1>
+      <MapContainer
+        center={[34.0522, -118.2437]}
+        zoom={12}
+        scrollWheelZoom={false}
+        id="map"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {activities.map((activity) => (
+          <Polyline key={activity.id} positions={activity.polyline}>
+            <Popup>
+              <div>
+                <h2>{activity.exercise + ": " + activity.title}</h2>
+              </div>
+            </Popup>
+          </Polyline>
+        ))}
+      </MapContainer>
+    </div>
+  );
 }
