94121ee30ca6d9afb47f6de5a16fb6197dbd2de3
Matt Moreno <morenomatthew83@gmail.com>
2024-07-15 00:09:28 -0700

    Case study component

---
+++ src/pages/Projects/views/Bellabeat.tsx
@@ -0,0 +1,9 @@
+export default function Bellabeat() {
+  return (
+    <iframe
+      src="https://www.kaggle.com/embed/morenomatt/bellabeat-case-study-r?kernelSessionId=180358905"
+      className="kaggle-iframe"
+      title="Bellabeat Case Study | R"
+    />
+  );
+}
