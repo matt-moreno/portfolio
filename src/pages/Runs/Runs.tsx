942a4ceb1819457e93858286629b3816b8f826cd
Matt Moreno <morenomatthew83@gmail.com>
2024-07-29 23:55:05 -0700

    Add recent activity

--- src/pages/Runs/Runs.tsx
+++ src/pages/Runs/Runs.tsx
@@ -1,36 +1,34 @@
 import { useState, useEffect } from "react";
 import { MapContainer, Popup, TileLayer, Polyline } from "react-leaflet";
 import { BarChart, Bar, ResponsiveContainer, XAxis, Tooltip } from "recharts";
+import {
+  convertMetersToMiles,
+  convertMetersPerSecondToMilesPerHour,
+  convertSecondsToFormattedTime,
+} from "./utils";
 import { CardInfo } from "../../components/Card/CardInfo";
-import { ActivityTypes } from "./RunTypes";
+import { weeklyTrackerData } from "./constants";
+import { GeoTypes, RecentActivityTypes, ActivityTypes } from "./RunTypes";
 import polyline from "@mapbox/polyline";
 import Loading from "../../components/Loading/Loading";
 import "./Runs.css";
 
-interface StravaTypes {
-  id: number;
-  title: string;
-  exercise: string;
-  elevHigh: number;
-  elevLow: number;
-  startLat: number[];
-  endLat: number[];
-  polyline: [number, number][];
-}
-
 export default function Runs() {
   const [loading, setLoading] = useState(true);
-  const [activities, setActivities] = useState<StravaTypes[]>([]);
+  const [geoData, setGeoData] = useState<GeoTypes[]>([]);
+  const [recentActivity, setRecentActivity] = useState<RecentActivityTypes[]>(
+    []
+  );
 
   useEffect(() => {
     const getStravaData = async () => {
       try {
         const response = await fetch("http://localhost:3000/api/stravaData");
         const result = await response.json();
-        const stravaArr: StravaTypes[] = [];
-        result.map((data: ActivityTypes) => {
-          console.log(data);
-          stravaArr.push({
+        const geoArr: GeoTypes[] = [];
+        const activityArr: RecentActivityTypes[] = [];
+        result.map((data: ActivityTypes, index: number) => {
+          geoArr.push({
             id: data.id,
             title: data.name,
             exercise: data.type,
@@ -40,8 +38,19 @@ export default function Runs() {
             endLat: data.end_latlng,
             polyline: polyline.decode(data.map.summary_polyline),
           });
+
+          if (index < 3) {
+            activityArr.push({
+              distance: convertMetersToMiles(data.distance),
+              pace: convertMetersPerSecondToMilesPerHour(data.average_speed),
+              time: convertSecondsToFormattedTime(data.moving_time),
+              heartRate: Math.round(data.average_heartrate),
+            });
+          }
         });
-        setActivities(stravaArr);
+        setGeoData(geoArr);
+        console.log(activityArr);
+        setRecentActivity(activityArr);
       } catch (error) {
         console.error("Error fetching data:", error);
       } finally {
@@ -52,38 +61,6 @@ export default function Runs() {
     getStravaData();
   }, []);
 
-  const data = [
-    {
-      name: "M",
-      miles: 5,
-    },
-    {
-      name: "T",
-      miles: 5,
-    },
-    {
-      name: "W",
-      miles: 10,
-    },
-    {
-      name: "T",
-      miles: 5,
-    },
-    {
-      name: "F",
-      miles: 10,
-    },
-    {
-      name: "S",
-      miles: 14,
-    },
-    {
-      name: "S",
-      miles: 5,
-    },
-  ];
-
-  // TODO: CREATE LOADING COMPONENT
   if (loading) {
     return <Loading />;
   }
@@ -105,11 +82,11 @@ export default function Runs() {
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            {activities.map((activity) => (
-              <Polyline key={activity.id} positions={activity.polyline}>
+            {geoData.map((geo) => (
+              <Polyline key={geo.id} positions={geo.polyline}>
                 <Popup>
                   <div>
-                    <h2>{activity.exercise + ": " + activity.title}</h2>
+                    <h2>{geo.exercise + ": " + geo.title}</h2>
                   </div>
                 </Popup>
               </Polyline>
@@ -117,23 +94,42 @@ export default function Runs() {
           </MapContainer>
           <div className="dashboard-top-right">
             <CardInfo className="weekly-tracker">
-              <p>Weekly tracker</p>
-              {/* TODO: Make height in ResponsiveContainer responsive  */}
+              <h2>Weekly tracker</h2>
               <ResponsiveContainer width="100%" height="80%">
-                <BarChart data={data}>
+                <BarChart data={weeklyTrackerData}>
                   <Tooltip />
                   <XAxis dataKey="name" tick={{ fill: "white" }} />
                   <Bar dataKey="miles" fill="#fc4c02" />
                 </BarChart>
               </ResponsiveContainer>
             </CardInfo>
-            <CardInfo className="marathons">Marathon Stars</CardInfo>
+            <CardInfo className="marathons">
+              <h2>Marathon Stars</h2>
+              <p>Star of different majors??</p>
+            </CardInfo>
           </div>
         </div>
 
         <div className="dashboard-bottom">
-          <CardInfo className="month-recap">Monthly Recap</CardInfo>
-          <CardInfo className="recent-activity">Recent Activity</CardInfo>
+          <CardInfo className="recap">
+            <h2>Recap</h2>
+            <p>Total miles this year, Total miles this week, June totals</p>
+            <ul>
+              <li>16 hrs</li>
+              <li>98.1 mi</li>
+            </ul>
+          </CardInfo>
+          <CardInfo className="recent-activity">
+            <h2>Recent Activity</h2>
+            {recentActivity.map((activity, index) => (
+              <div className="activity-container" key={index}>
+                <p>Distance: {activity.distance}/mi</p>
+                <p>Avg Pace: {activity.pace}/mi</p>
+                <p>Time: {activity.time}</p>
+                <p>Avg Heart Rate: {activity.heartRate} bpm</p>
+              </div>
+            ))}
+          </CardInfo>
         </div>
       </section>
     </div>
