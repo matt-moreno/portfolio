22c245497e1fe75894230d0057219ca244464b3f
Matt Moreno <morenomatthew83@gmail.com>
2024-07-29 23:50:54 -0700

    move temporary tracker data

---
+++ src/pages/Runs/constants.ts
@@ -0,0 +1,32 @@
+const weeklyTrackerData = [
+  {
+    name: "M",
+    miles: 5,
+  },
+  {
+    name: "T",
+    miles: 5,
+  },
+  {
+    name: "W",
+    miles: 10,
+  },
+  {
+    name: "T",
+    miles: 5,
+  },
+  {
+    name: "F",
+    miles: 10,
+  },
+  {
+    name: "S",
+    miles: 14,
+  },
+  {
+    name: "S",
+    miles: 5,
+  },
+];
+
+export { weeklyTrackerData };
