8866954f0610b78af1397e068016193226c777b8
matt-moreno <morenomatthew83@gmail.com>
2025-05-05 21:07:43 -0700

    fix resources

--- src/pages/Resources/Resources.tsx
+++ src/pages/Resources/Resources.tsx
@@ -4,17 +4,19 @@ import "./Resources.css";
 export default function Resources() {
   return (
     <div className="main-wrapper">
-      <h1>Recommendations</h1>
-      <nav className="resources-nav">
-        <NavLink to="." end>
-          Websites
-        </NavLink>
-        <NavLink to="/resources/videos">Videos</NavLink>
-        <NavLink to="/resources/books">Books</NavLink>
-      </nav>
-      <div>
-        <Outlet />
-      </div>
+      <section className="resources-container">
+        <h1>Recommendations</h1>
+        <nav className="resources-nav">
+          <NavLink to="." end>
+            Websites
+          </NavLink>
+          <NavLink to="/resources/videos">Videos</NavLink>
+          <NavLink to="/resources/books">Books</NavLink>
+        </nav>
+        <div>
+          <Outlet />
+        </div>
+      </section>
     </div>
   );
 }
