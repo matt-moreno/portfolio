8bbe2b2b4bbbb87b93a4f3e0ac1c4282feb6be99
matt-moreno <morenomatthew83@gmail.com>
2025-05-20 22:48:42 -0700

    clean backend

--- api/app.js
+++ api/app.js
@@ -1,56 +1,31 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable no-undef */
-const express = require("express");
-const cors = require("cors");
+/* global process */
+import express from "express";
+import cors from "cors";
+import stravaRoutes from "./routes/strava.js";
+
 const app = express();
-const port = 3000;
-require("dotenv").config();
+const port = process.env.PORT || 3000;
 
 const corsOptions = {
   origin: "http://localhost:5173",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(cors(corsOptions));
+app.use(express.json());
+app.use("/api", stravaRoutes);
 
-app.get("/api/stravaData", (req, res) => {
-  const refreshAccessToken = async () => {
-    try {
-      const response = await fetch("https://www.strava.com/oauth/token", {
-        method: "post",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          client_id: process.env.STRAVA_CLIENT_ID,
-          client_secret: process.env.STRAVA_SECRET,
-          code: process.env.STRAVA_AUTH_CODE,
-          grant_type: "refresh_token",
-          refresh_token: process.env.STRAVA_REFRESH_TOKEN,
-        }),
-      });
-      const result = await response.json();
-      fetchActivityData(result.access_token);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  const fetchActivityData = async (token) => {
-    try {
-      const response = await fetch(
-        `https://www.strava.com/api/v3/activities?access_token=${token}`
-      );
-      const result = await response.json();
-      res.send(result);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  refreshAccessToken();
+// Error handling middleware
+app.use((error, request, response) => {
+  console.error(error.stack);
+  response.status(500).json({
+    error: "Internal Server Error",
+    message: "Something went wrong!",
+  });
 });
 
+// Start server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
