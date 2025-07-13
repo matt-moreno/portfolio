931fc171c99aa6597becbb96a5cb1f46dd244510
Matt Moreno <88680203+matt-moreno@users.noreply.github.com>
2025-07-12 21:05:27 -0700

    Dashboard improvements (#22)
    * dash improv
    
    * add vercel.json

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -2,7 +2,7 @@ import ReactLoading from "react-loading";
 
 export default function Loading() {
   return (
-    <div className="absolute inset-0 flex flex-col items-center justify-center md:left-[300px]">
+    <div className="absolute inset-0 flex flex-col items-center justify-center md:left-[300px] bg-gradient-to-br from-slate-50 to-slate-100 dark:from-slate-900 dark:to-slate-800">
       <ReactLoading type="bars" />
       <h1>Loading</h1>
     </div>
